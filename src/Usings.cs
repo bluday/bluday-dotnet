c7a958ac5ef53afc41c41038fbdf9f1b95e07374
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-13 14:17:52 +0100

    feat: add back property and argument extension classes and rename parser class

--- src/Usings.cs
+++ src/Usings.cs
@@ -1,6 +1,7 @@
 global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Messaging;
 global using BluDay.Net.Common.Attributes;
+global using BluDay.Net.Common.CommandLine;
 global using BluDay.Net.Common.Extensions;
 global using BluDay.Net.UI.Appearance;
 global using BluDay.Net.UI.Navigation;
