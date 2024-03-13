cae6e81a1d2e15838ecc9edde182925639200d43
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-13 14:21:42 +0100

    feat: renmae generic files according to new convention and fix some syntaxes

--- src/Common/Attributes/UseViewModelAttribute.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BluDay.Net.Common.Attributes;
-
-public sealed class UseViewModelAttribute<TViewModel> : Attribute where TViewModel : IViewModel
-{
-    public static Type TargetType { get; } = typeof(TViewModel);
-}
