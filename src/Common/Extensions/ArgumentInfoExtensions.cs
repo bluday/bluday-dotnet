90d55bdde0901a2701a60f8e310d0d5937449849
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-13 14:18:24 +0100

    feat: add untracked files

---
+++ src/Common/Extensions/ArgumentInfoExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace BluDay.Net.Common.Extensions;
+
+public static class ArgumentInfoExtensions
+{
+    public static Dictionary<ArgumentInfo, PropertyInfo> CreateArgumentToPropertyMap<TArgs>(
+        this IEnumerable<ArgumentInfo> arguments
+    )
+        where TArgs : IArgs, new()
+    {
+        return typeof(TArgs)
+            .GetProperties()
+            .Select(
+                property => (
+                    Property: property,
+                    ArgInfo:  property.GetArgument(arguments)
+                )
+            )
+            .Where(
+                pair => pair.ArgInfo is not null
+            )
+            .ToDictionary(
+                pair => pair.ArgInfo!,
+                pair => pair.Property
+            );
+    }
+}
---
+++ src/Common/Extensions/PropertyInfoExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace BluDay.Net.Common.Extensions;
+
+public static class PropertyInfoExtensions
+{
+    public static ArgumentInfo? GetArgument(this PropertyInfo source, IEnumerable<ArgumentInfo> arguments)
+    {
+        return arguments.FirstOrDefault(argument => GetTargetedArgumentName(source) == argument.Name);
+    }
+
+    public static string? GetTargetedArgumentName(this PropertyInfo source)
+    {
+        ArgumentAttribute? attribute = source.GetCustomAttribute<ArgumentAttribute>();
+
+        return attribute?.TargetName ?? source.Name;
+    }
+}
