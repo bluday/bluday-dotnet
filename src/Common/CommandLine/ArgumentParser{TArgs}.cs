b36fecd1a311d16456a227684be39fafd924f6bb
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-16 00:51:55 +0100

    feat: remove method extension classes and use more complete methods instead

--- src/Common/CommandLine/ArgumentParser{TArgs}.cs
+++ src/Common/CommandLine/ArgumentParser{TArgs}.cs
@@ -15,9 +15,52 @@ public class ArgumentParser<TArgs> where TArgs : IArgs, new()
 
     public ArgumentParser(IEnumerable<ArgumentInfo> arguments)
     {
-        _argumentToPropertyMap = arguments
-            .CreateArgumentToPropertyMap<TArgs>()
-            .AsReadOnly();
+        _argumentToPropertyMap = CreateArgumentToPropertyMap(arguments);
+    }
+
+    private static ArgumentInfo? GetArgumentByName(string name, IEnumerable<ArgumentInfo> arguments)
+    {
+        return arguments.FirstOrDefault(argument => argument.Name == name);
+    }
+
+    private static IReadOnlyDictionary<ArgumentInfo, PropertyInfo> CreateArgumentToPropertyMap(IEnumerable<ArgumentInfo> arguments)
+    {
+        Dictionary<ArgumentInfo, PropertyInfo> map = new();
+
+        PropertyInfo[] properties = typeof(TArgs).GetProperties();
+
+        foreach (var property in properties)
+        {
+            ArgumentAttribute? attribute = property.GetCustomAttribute<ArgumentAttribute>();
+
+            if (attribute is null) continue;
+
+            string targetedArgumentName = attribute.TargetName ?? property.Name;
+
+            ArgumentInfo? argument = GetArgumentByName(targetedArgumentName, arguments);
+
+            if (argument is null)
+            {
+                // TODO: Throw non-found argument info exception.
+                throw new ArgumentException();
+            }
+
+            if (map.ContainsKey(argument))
+            {
+                // TODO: Throw already-evaluted argument exception.
+                throw new InvalidOperationException();
+            }
+
+            if (map.ContainsValue(property))
+            {
+                // TODO: Throw target property conflict exception.
+                throw new InvalidOperationException();
+            }
+
+            map[argument!] = property;
+        }
+
+        return map.AsReadOnly();
     }
 
     public object? ParseArgument(ArgumentInfo info, string value)
