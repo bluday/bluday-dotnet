2b0f6dd82b665248b6b24795753c707687307282
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-16 01:28:44 +0100

    feat: add new ParsedArgument class

---
+++ src/Common/CommandLine/ParsedArgument.cs
@@ -0,0 +1,24 @@
+namespace BluDay.Net.Common.CommandLine;
+
+public sealed class ParsedArgument
+{
+    public ArgumentInfo Info { get; }
+
+    public ArgumentToken Token { get; }
+
+    public object[] Values { get; }
+
+    public ParsedArgument(ArgumentInfo info, ArgumentToken token) : this(info, token, []) { }
+
+    public ParsedArgument(ArgumentInfo info, ArgumentToken token, object[] values)
+    {
+        ArgumentNullException.ThrowIfNull(info);
+        ArgumentNullException.ThrowIfNull(values);
+
+        // TODO: Validate token—and values if not empty.
+
+        Info   = info;
+        Token  = token;
+        Values = values;
+    }
+}
