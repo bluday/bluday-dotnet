dc6e2f4bcb7fa197ed3e0c4ee21e611bbe777f45
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-19 19:37:13 +0100

    feat: create new method extension class and move methods for now

--- src/BluDay.Net/Common/CommandLine/ArgumentParser{TArgs}.cs
+++ src/BluDay.Net/Common/CommandLine/ArgumentParser{TArgs}.cs
@@ -16,46 +16,7 @@ public class ArgumentParser<TArgs> where TArgs : IArgs, new()
 
     public ArgumentParser(IEnumerable<ArgumentInfo> arguments)
     {
-        _argumentToPropertyMap = CreateArgumentToPropertyMap(arguments);
-    }
-
-    private static ArgumentInfo? GetArgument(string name, IEnumerable<ArgumentInfo> arguments)
-    {
-        return arguments.FirstOrDefault(argument => argument.Name == name);
-    }
-
-    private static Dictionary<ArgumentInfo, PropertyInfo> CreateArgumentToPropertyMap(IEnumerable<ArgumentInfo> arguments)
-    {
-        Dictionary<ArgumentInfo, PropertyInfo> map = new();
-
-        PropertyInfo[] properties = typeof(TArgs).GetProperties();
-
-        foreach (var property in properties)
-        {
-            ArgumentAttribute? attribute = property.GetCustomAttribute<ArgumentAttribute>();
-
-            if (attribute is null) continue;
-
-            string name = attribute.TargetName ?? property.Name;
-
-            ArgumentInfo? argument = GetArgument(name, arguments);
-
-            if (argument is null)
-            {
-                // TODO: Throw non-found argument info exception.
-                throw new ArgumentException();
-            }
-
-            if (map.ContainsValue(property))
-            {
-                // TODO: Throw target property conflict exception.
-                throw new InvalidOperationException();
-            }
-
-            map.Add(argument!, property);
-        }
-
-        return map;
+        _argumentToPropertyMap = arguments.CreateArgumentToPropertyMap<TArgs>();
     }
 
     public object? Parse(ArgumentInfo info, string arg)
---
+++ src/BluDay.Net/Common/Extensions/ArgumentInfoExtensions.cs
@@ -0,0 +1,46 @@
+﻿namespace BluDay.Net.Common.Extensions;
+
+public static class ArgumentInfoExtensions
+{
+    public static ArgumentInfo? GetArgumentByName(this IEnumerable<ArgumentInfo> source, string name)
+    {
+        return source.FirstOrDefault(argument => argument.Name == name);
+    }
+
+    public static Dictionary<ArgumentInfo, PropertyInfo> CreateArgumentToPropertyMap<TArgs>(
+        this IEnumerable<ArgumentInfo> source
+    )
+        where TArgs : IArgs, new()
+    {
+        Dictionary<ArgumentInfo, PropertyInfo> map = new();
+
+        PropertyInfo[] properties = typeof(TArgs).GetProperties();
+
+        foreach (var property in properties)
+        {
+            ArgumentAttribute? attribute = property.GetCustomAttribute<ArgumentAttribute>();
+
+            if (attribute is null) continue;
+
+            string name = attribute.TargetName ?? property.Name;
+
+            ArgumentInfo? argument = source.GetArgumentByName(name);
+
+            if (argument is null)
+            {
+                // TODO: Throw non-found argument info exception.
+                throw new ArgumentException();
+            }
+
+            if (map.ContainsValue(property))
+            {
+                // TODO: Throw target property conflict exception.
+                throw new InvalidOperationException();
+            }
+
+            map.Add(argument!, property);
+        }
+
+        return map;
+    }
+}
