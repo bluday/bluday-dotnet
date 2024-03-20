a69c8492f84c63567b50de84bac1c9eac8548163
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-20 19:19:18 +0100

    chore: clear implementation for both exceptions classes for now

--- src/BluDay.Net/Common/Exceptions/InvalidArgumentFlagException.cs
+++ src/BluDay.Net/Common/Exceptions/InvalidArgumentFlagException.cs
@@ -2,5 +2,5 @@ namespace BluDay.Net.Common.Exceptions;
 
 public sealed class InvalidArgumentFlagException : Exception
 {
-    public InvalidArgumentFlagException(string flag, string message) : base(message) { }
+    public InvalidArgumentFlagException(string value) { }
 }
--- src/BluDay.Net/Common/Exceptions/UnrecognizedArgumentException.cs
+++ src/BluDay.Net/Common/Exceptions/UnrecognizedArgumentException.cs
@@ -2,6 +2,5 @@ namespace BluDay.Net.Common.Exceptions;
 
 public sealed class UnrecognizedArgumentException : Exception
 {
-    public UnrecognizedArgumentException(string flag)
-        : base($"Unrecognized argument \"{flag}\" has not been registered.") { }
+    public UnrecognizedArgumentException(string value) { }
 }
