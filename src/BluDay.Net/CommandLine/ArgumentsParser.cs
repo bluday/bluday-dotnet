bd1ba4ab81d53dbba6f33e69e87a1d550b16d958
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-24 15:19:26 +0100

    feat: make generic arguments parser class non-generic

--- src/BluDay.Net/CommandLine/ArgumentsParser.cs
+++ src/BluDay.Net/CommandLine/ArgumentsParser.cs
@@ -1,8 +1,14 @@
 ﻿namespace BluDay.Net.CommandLine;
 
-public abstract class ArgumentsParser
+public sealed class ArgumentsParser
 {
+    private readonly IEnumerable<Argument> _availableArguments;
+
+    private readonly IReadOnlyDictionary<Argument, PropertyInfo> _argumentToPropertyMap;
+
     internal static readonly BindingFlags _propertyBindingFlags;
+
+    public IEnumerable<Argument> AvailableArguments => _availableArguments;
 
     static ArgumentsParser()
     {
@@ -10,5 +16,51 @@ public abstract class ArgumentsParser
             | BindingFlags.Instance
             | BindingFlags.Public
             | BindingFlags.SetProperty;
+    }
+
+    public ArgumentsParser(IEnumerable<Argument> arguments)
+    {
+        _availableArguments = arguments;
+
+        _argumentToPropertyMap = null!; // CreateArgumentToPropertyMap(arguments);
+    }
+
+    /*
+    private static IReadOnlyDictionary<Argument, PropertyInfo> CreateArgumentToPropertyMap(IEnumerable<Argument> arguments)
+    {
+        Dictionary<Argument, PropertyInfo> map = new();
+
+        PropertyInfo[] properties = typeof(TArguments).GetProperties(_propertyBindingFlags);
+
+        foreach (var property in properties)
+        {
+            ArgumentAttribute? attribute = property.GetCustomAttribute<ArgumentAttribute>();
+
+            if (attribute is null) continue;
+
+            string argumentName = attribute.TargetName ?? property.Name;
+
+            Argument? argument = arguments.First(argument => argument.Name == argumentName);
+
+            if (map.ContainsValue(property))
+            {
+                throw new ConflictingArgumentTargetPropertyException(property);
+            }
+
+            map.Add(argument, property);
+        }
+
+        return map;
+    }
+    */
+
+    public object Parse(string[] args)
+    {
+        throw new NotImplementedException();
+    }
+
+    public TArguments Parse<TArguments>(string[] args) where TArguments : new()
+    {
+        return (TArguments)Parse(args);
     }
 }
