b6d1fb8f744f635d73e91ce327e1c9f3a53026c3
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-24 17:35:20 +0100

    feat: use method instead of field

--- src/BluDay.Net/CommandLine/ArgumentsParser.cs
+++ src/BluDay.Net/CommandLine/ArgumentsParser.cs
@@ -4,21 +4,19 @@ public class ArgumentsParser
 {
     private readonly IEnumerable<Argument> _arguments;
 
-    internal static readonly BindingFlags _propertyBindingFlags;
-
     public IEnumerable<Argument> Arguments => _arguments;
-
-    static ArgumentsParser()
-    {
-        _propertyBindingFlags = BindingFlags.DeclaredOnly
-            | BindingFlags.Instance
-            | BindingFlags.Public
-            | BindingFlags.SetProperty;
-    }
 
     public ArgumentsParser(IEnumerable<Argument> arguments)
     {
         _arguments = arguments;
+    }
+
+    internal static BindingFlags GetTargetPropertyBindingFlags()
+    {
+        return BindingFlags.DeclaredOnly
+            | BindingFlags.Instance
+            | BindingFlags.Public
+            | BindingFlags.SetProperty;
     }
 
     public object Parse(Type objectType, string[] args)
