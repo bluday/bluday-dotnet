d946de21ee122aa88a8a8daa008cfcae4d9be8fc
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-24 15:45:42 +0100

    feat: remove old args-parsing implementation and rename field and property

--- src/BluDay.Net/CommandLine/ArgumentsParser.cs
+++ src/BluDay.Net/CommandLine/ArgumentsParser.cs
@@ -2,13 +2,11 @@
 
 public sealed class ArgumentsParser
 {
-    private readonly IEnumerable<Argument> _availableArguments;
-
-    private readonly IReadOnlyDictionary<Argument, PropertyInfo> _argumentToPropertyMap;
+    private readonly IEnumerable<Argument> _arguments;
 
     internal static readonly BindingFlags _propertyBindingFlags;
 
-    public IEnumerable<Argument> AvailableArguments => _availableArguments;
+    public IEnumerable<Argument> Arguments => _arguments;
 
     static ArgumentsParser()
     {
@@ -20,39 +18,8 @@ public sealed class ArgumentsParser
 
     public ArgumentsParser(IEnumerable<Argument> arguments)
     {
-        _availableArguments = arguments;
-
-        _argumentToPropertyMap = null!; // CreateArgumentToPropertyMap(arguments);
+        _arguments = arguments;
     }
-
-    /*
-    private static IReadOnlyDictionary<Argument, PropertyInfo> CreateArgumentToPropertyMap(IEnumerable<Argument> arguments)
-    {
-        Dictionary<Argument, PropertyInfo> map = new();
-
-        PropertyInfo[] properties = typeof(TArguments).GetProperties(_propertyBindingFlags);
-
-        foreach (var property in properties)
-        {
-            ArgumentAttribute? attribute = property.GetCustomAttribute<ArgumentAttribute>();
-
-            if (attribute is null) continue;
-
-            string argumentName = attribute.TargetName ?? property.Name;
-
-            Argument? argument = arguments.First(argument => argument.Name == argumentName);
-
-            if (map.ContainsValue(property))
-            {
-                throw new ConflictingArgumentTargetPropertyException(property);
-            }
-
-            map.Add(argument, property);
-        }
-
-        return map;
-    }
-    */
 
     public TArguments Parse<TArguments>(string[] args) where TArguments : new()
     {
