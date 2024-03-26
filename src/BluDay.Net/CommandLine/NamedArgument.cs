6b5cf74b026fe8052dc1af74df68f1657878e3fb
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-26 14:24:49 +0100

    feat: rehaul NamedArgument

--- src/BluDay.Net/CommandLine/NamedArgument.cs
+++ src/BluDay.Net/CommandLine/NamedArgument.cs
@@ -2,14 +2,35 @@ namespace BluDay.Net.CommandLine;
 
 public sealed class NamedArgument : Argument
 {
-    public ArgumentFlag? ShortFlag { get; }
+    private readonly ArgumentFlag? _longFlag, _shortFlag;
 
-    public ArgumentFlag? LongFlag { get; }
+    public ArgumentFlag PrimaryFlag => (_shortFlag ?? _longFlag)!.Value;
 
-    public NamedArgument(string flagDescriptor)
+    public ArgumentFlag? LongFlag => _longFlag;
+
+    public ArgumentFlag? ShortFlag => _shortFlag;
+
+    public NamedArgument(string flagName)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(flagDescriptor);
+        ArgumentException.ThrowIfNullOrWhiteSpace(flagName);
 
-        // TODO: Parse flag descriptor string and create short and long flag instances.
+        if (flagName.Length > 1)
+        {
+            _longFlag = new(flagName, ArgumentFlagType.Long);
+        }
+        else
+        {
+            _shortFlag = new(flagName, ArgumentFlagType.Short);
+        }
+    }
+
+    public NamedArgument(string shortFlagName, string longFlagName)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(longFlagName);
+        ArgumentException.ThrowIfNullOrWhiteSpace(shortFlagName);
+
+        _longFlag = new(longFlagName, ArgumentFlagType.Long);
+
+        _shortFlag = new(shortFlagName, ArgumentFlagType.Short);
     }
 }
