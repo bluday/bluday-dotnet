a9381e356b93884ec4c47f07ab32fa0b45a27752
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-26 16:48:07 +0100

    feat: add back method extension class for ArgumentsParser<TArguments>

---
+++ src/BluDay.Net/Common/Extensions/ArgumentsParserExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace BluDay.Net.Common.Extensions;
+
+public static class ArgumentsParserExtensions
+{
+    public static TArguments ParseFromCommandLine<TArguments>(this ArgumentsParser<TArguments> source)
+        where TArguments : new()
+    {
+        string[] args = Environment.GetCommandLineArgs()[1..];
+
+        return source.Parse(args);
+    }
+}
