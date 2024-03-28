2e7f8e032c556d61f541c28ce9fba1d673b2c802
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-28 14:51:26 +0100

    chore: set symbol and name in constructor

--- src/BluDay.Net/CommandLine/PositionalArgumentDescriptor.cs
+++ src/BluDay.Net/CommandLine/PositionalArgumentDescriptor.cs
@@ -2,5 +2,12 @@ namespace BluDay.Net.CommandLine;
 
 public sealed class PositionalArgumentDescriptor : ArgumentDescriptor
 {
-    public string Symbol { get; } = Constants.POSITIONAL_ARG_SYMBOL;
+    public string Symbol { get; }
+
+    public PositionalArgumentDescriptor()
+    {
+        Name = nameof(PositionalArgumentDescriptor);
+
+        Symbol = Constants.POSITIONAL_ARG_SYMBOL;
+    }
 }
