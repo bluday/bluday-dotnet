cf23ddf6c166f13f644a6d491101087896467094
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-29 01:04:56 +0100

    feat: remove Descriptor suffix

---
+++ src/BluDay.Net/Infrastructure/CommandLine/Argument.cs
@@ -0,0 +1,22 @@
+namespace BluDay.Net.CommandLine;
+
+public abstract class Argument : IArgument
+{
+    public ArgumentActionType ActionType { get; init; }
+
+    public ArgumentStoreType StoreType { get; init; }
+
+    public bool IsRequired { get; init; }
+
+    public object? Constant { get; init; }
+
+    public object? DefaultValue { get; init; }
+
+    public string? Description { get; init; }
+
+    public virtual string Name { get; init; } = null!;
+
+    public int MaxValueCount { get; init; }
+
+    public Func<string, object?> ValueHandler { get; init; } = null!;
+}
---
+++ src/BluDay.Net/Infrastructure/CommandLine/IArgument.cs
@@ -0,0 +1,22 @@
+namespace BluDay.Net.CommandLine;
+
+public interface IArgument
+{
+    ArgumentActionType ActionType { get; }
+
+    ArgumentStoreType StoreType { get; }
+
+    bool IsRequired { get; }
+
+    object? Constant { get; }
+
+    object? DefaultValue { get; }
+
+    string? Description { get; }
+
+    string Name { get; }
+
+    int MaxValueCount { get; }
+
+    Func<string, object?> ValueHandler { get; }
+}
