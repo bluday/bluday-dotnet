fe37cdd1b01447c63955ca845dbc44badf33ac08
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-31 00:14:23 +0100

    feat: use init accessors instead of cumbersome constructor

--- src/BluDay.Net/CommandLine/ArgumentsParser{TArguments}.cs
+++ src/BluDay.Net/CommandLine/ArgumentsParser{TArguments}.cs
@@ -8,19 +8,19 @@ public class ArgumentsParser<TArguments> where TArguments : new()
 
     public PositionalArgument? PositionalArgument
     {
-        get => _positionalArgument;
+        get  => _positionalArgument;
+        init => _positionalArgument = value;
     }
 
     public IImmutableList<OptionalArgument> OptionalArguments
     {
-        get => _optionalArguments;
+        get  => _optionalArguments;
+        init => _optionalArguments = value.Distinct().ToImmutableList();
     }
 
-    public ArgumentsParser(IEnumerable<OptionalArgument> optionalArguments, PositionalArgument? positionalArgument)
+    public ArgumentsParser()
     {
-        _positionalArgument = positionalArgument;
-
-        _optionalArguments = optionalArguments.Distinct().ToImmutableList();
+        _optionalArguments = ImmutableList<OptionalArgument>.Empty;
     }
 
     internal static BindingFlags GetTargetPropertyReflectionBindingFlags()
