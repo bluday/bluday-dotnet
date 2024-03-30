87cd53cdcffbfe5bfe66975da752e30a22a2e8d9
BluDay <19403893+BluDay@users.noreply.github.com>
2024-03-31 00:42:45 +0100

    chore: explicit parameter-less constructor and set default value for _values

--- src/BluDay.Net/CommandLine/ParsedArgument.cs
+++ src/BluDay.Net/CommandLine/ParsedArgument.cs
@@ -1,17 +1,24 @@
 namespace BluDay.Net.CommandLine;
 
-public readonly struct ParsedArgument(ArgumentToken token)
+public readonly struct ParsedArgument
 {
     private readonly IImmutableList<ArgumentToken> _values;
 
-    public ArgumentToken Token { get; } = token;
+    public ArgumentToken Token { get; }
 
-    public bool HasValues => _values is not null;
+    public bool HasValues => _values.Count > 0;
 
     [DisallowNull]
     public IImmutableList<ArgumentToken> Values
     {
         get  => _values;
         init => _values = value;
+    }
+
+    public ParsedArgument(ArgumentToken token)
+    {
+        _values = ImmutableList<ArgumentToken>.Empty;
+
+        Token = token;
     }
 }
