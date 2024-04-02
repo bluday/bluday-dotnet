830ed30170df225e6c79504421d0a321c57b4146
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-02 14:25:08 +0200

    feat: correct constructors

--- src/BluDay.Net/BluDay.Net/CommandLine/ArgumentsParser{TArguments}.cs
+++ src/BluDay.Net/BluDay.Net/CommandLine/ArgumentsParser{TArguments}.cs
@@ -21,21 +21,26 @@ public class ArgumentsParser<TArguments> where TArguments : new()
         positionalArgument: null!
     ) { }
 
-    public ArgumentsParser(IEnumerable<OptionalArgument> optionalArguments) : this(
-        optionalArguments:  optionalArguments,
-        positionalArgument: null!
-    ) { }
-
     public ArgumentsParser(PositionalArgument positionalArgument) : this(
         optionalArguments:  null!,
         positionalArgument: positionalArgument
+    ) { }
+
+    public ArgumentsParser(IEnumerable<OptionalArgument> optionalArguments) : this(
+        optionalArguments:  optionalArguments,
+        positionalArgument: null!
     ) { }
 
     public ArgumentsParser(IEnumerable<OptionalArgument> optionalArguments, PositionalArgument positionalArgument)
     {
         _positionalArgument = positionalArgument;
 
-        optionalArguments ??= ImmutableList<OptionalArgument>.Empty!;
+        if (optionalArguments is null)
+        {
+            _optionalArguments = ImmutableList<OptionalArgument>.Empty;
+
+            return;
+        }
 
         _optionalArguments = optionalArguments.Distinct().ToImmutableList();
     }
