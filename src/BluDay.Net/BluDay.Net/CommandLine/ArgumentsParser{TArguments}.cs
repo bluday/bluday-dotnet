e7f20c013fa09cdecf72c2e0b3d21a1dd5e13105
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-02 13:50:30 +0200

    feat: add new chained constructor overloads

--- src/BluDay.Net/BluDay.Net/CommandLine/ArgumentsParser{TArguments}.cs
+++ src/BluDay.Net/BluDay.Net/CommandLine/ArgumentsParser{TArguments}.cs
@@ -16,9 +16,26 @@ public class ArgumentsParser<TArguments> where TArguments : new()
         get => _optionalArguments;
     }
 
-    public ArgumentsParser(IEnumerable<OptionalArgument> optionalArguments, PositionalArgument? positionalArgument)
+    public ArgumentsParser() : this(
+        optionalArguments:  null!,
+        positionalArgument: null!
+    ) { }
+
+    public ArgumentsParser(IEnumerable<OptionalArgument> optionalArguments) : this(
+        optionalArguments:  optionalArguments,
+        positionalArgument: null!
+    ) { }
+
+    public ArgumentsParser(PositionalArgument positionalArgument) : this(
+        optionalArguments:  null!,
+        positionalArgument: positionalArgument
+    ) { }
+
+    public ArgumentsParser(IEnumerable<OptionalArgument> optionalArguments, PositionalArgument positionalArgument)
     {
         _positionalArgument = positionalArgument;
+
+        optionalArguments ??= ImmutableList<OptionalArgument>.Empty!;
 
         _optionalArguments = optionalArguments.Distinct().ToImmutableList();
     }
