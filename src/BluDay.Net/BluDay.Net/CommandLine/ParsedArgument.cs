266c9935ccdb7b802e9ed5ba1574549bbce3537d
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-02 16:03:47 +0200

    feat: add XML comments for ParsedArgument struct and init _values properly

--- src/BluDay.Net/BluDay.Net/CommandLine/ParsedArgument.cs
+++ src/BluDay.Net/BluDay.Net/CommandLine/ParsedArgument.cs
@@ -1,22 +1,41 @@
 namespace BluDay.Net.CommandLine;
 
+/// <summary>
+/// A struct for representing a raw command-line argument value.
+/// </summary>
 public readonly struct ParsedArgument
 {
     private readonly IImmutableList<ArgumentToken> _values;
 
+    /// <summary>
+    /// The token of the parsed argument.
+    /// </summary>
     public ArgumentToken Token { get; }
 
+    /// <summary>
+    /// A convenience property for evaluating the value count for the parsed argument.
+    /// </summary>
     public bool HasValues => _values.Count > 0;
 
+    /// <summary>
+    /// An immutable list of store value tokens.
+    /// </summary>
     public IImmutableList<ArgumentToken> Values => _values;
 
-    public ParsedArgument(ArgumentToken token) : this(token, []) { }
+    /// <summary>
+    /// Initializes a new <see cref="ParsedArgument"/> value without any store values.
+    /// </summary>
+    /// <param name="token">The token for the parsed argument.</param>
+    public ParsedArgument(ArgumentToken token) : this(token, null!) { }
 
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="token">The token for the parsed argument.</param>
+    /// <param name="values">An enumerable of store value tokens.</param>
     public ParsedArgument(ArgumentToken token, IEnumerable<ArgumentToken> values)
     {
-        ArgumentNullException.ThrowIfNull(values);
-
-        _values = values.ToImmutableList();
+        _values = values.ToImmutableList() ?? ImmutableList<ArgumentToken>.Empty;
 
         Token = token;
     }
