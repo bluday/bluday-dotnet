9093f566af62364ea539a01e419721374a71c77b
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-02 15:24:53 +0200

    feat: add XML comments to ArgumentsParser<TArguments> class

--- src/BluDay.Net/BluDay.Net/CommandLine/ArgumentsParser{TArguments}.cs
+++ src/BluDay.Net/BluDay.Net/CommandLine/ArgumentsParser{TArguments}.cs
@@ -1,41 +1,70 @@
 ﻿namespace BluDay.Net.CommandLine;
 
+/// <summary>
+/// A class that facilitates parsing and mapping of command-line argument values to an instance of the specified generic parameter type.
+/// </summary>
+/// <typeparam name="TArguments">The target type for argument mapping.</typeparam>
 public class ArgumentsParser<TArguments> where TArguments : new()
 {
-    private readonly PositionalArgument? _positionalArgument;
+    private readonly PositionalArgument? _positional;
 
-    private readonly IImmutableList<OptionalArgument> _optionalArguments;
+    private readonly IImmutableList<OptionalArgument> _optionals;
 
-    public PositionalArgument? PositionalArgument
-    {
-        get => _positionalArgument;
-    }
+    /// <summary>
+    /// Represents the positional argument.
+    /// </summary>
+    public PositionalArgument? PositionalArgument => _positional;
 
-    public IImmutableList<OptionalArgument> OptionalArguments
-    {
-        get => _optionalArguments;
-    }
+    /// <summary>
+    /// Gets an immutable list of distinct optional arguments.
+    /// </summary>
+    public IImmutableList<OptionalArgument> OptionalArguments => _optionals;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArgumentsParser{TArguments}"/> class with default values.
+    /// </summary>
     public ArgumentsParser() : this(null!, null!) { }
 
-    public ArgumentsParser(PositionalArgument positionalArgument) : this(null!, positionalArgument) { }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArgumentsParser{TArguments}"/> class with a positional argument.
+    /// </summary>
+    /// <param name="positional">The postional argument.</param>
+    public ArgumentsParser(PositionalArgument positional) : this(null!, positional) { }
 
-    public ArgumentsParser(IEnumerable<OptionalArgument> optionalArguments) : this(optionalArguments, null!) { }
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArgumentsParser{TArguments}"/> class with optional arguments.
+    /// </summary>
+    /// <param name="optionals">An enumerable of optionals arguments.</param>
+    public ArgumentsParser(IEnumerable<OptionalArgument> optionals) : this(optionals, null!) { }
 
-    public ArgumentsParser(IEnumerable<OptionalArgument> optionalArguments, PositionalArgument positionalArgument)
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArgumentsParser{TArguments}"/> class with default values.
+    /// </summary>
+    /// <param name="optionals">An enumerable of optionals arguments.</param>
+    /// <param name="positional">The postional argument.</param>
+    public ArgumentsParser(IEnumerable<OptionalArgument> optionals, PositionalArgument positional)
     {
-        _positionalArgument = positionalArgument;
+        _positional = positional;
 
-        _optionalArguments = optionalArguments?
+        _optionals = optionals?
             .Distinct()
             .ToImmutableList() ?? ImmutableList<OptionalArgument>.Empty;
     }
 
+    /// <summary>
+    /// Parses raw argument string values and maps them to a new <see cref="TArguments"/> instance.
+    /// </summary>
+    /// <param name="values">Raw argument values.</param>
+    /// <returns>A new <see cref="TArguments"/> instance with parsed and mapped argument values.</returns>
     public TArguments Parse(params string[] values)
     {
         throw new NotImplementedException();
     }
 
+    /// <summary>
+    /// Convenient method for parsing arguments using <see cref="Environment.GetCommandLineArgs"/>.
+    /// </summary>
+    /// <returns>A new <see cref="TArguments"/> instance with parsed and mapped argument values.</returns>
     public TArguments ParseFromCommandLine()
     {
         return Parse(Environment.GetCommandLineArgs());
