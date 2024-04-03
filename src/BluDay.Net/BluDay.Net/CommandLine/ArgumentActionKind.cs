de4ff01f1b1e7bdf94f1b897ba1b1243dba65fe1
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-03 16:10:36 +0200

    chore: add xml documentation for ArgumentActionKind

--- src/BluDay.Net/BluDay.Net/CommandLine/ArgumentActionKind.cs
+++ src/BluDay.Net/BluDay.Net/CommandLine/ArgumentActionKind.cs
@@ -1,12 +1,42 @@
 namespace BluDay.Net.CommandLine;
 
+/// <summary>
+/// Represents different actions related to command-line arguments.
+/// </summary>
 public enum ArgumentActionKind
 {
+    /// <summary>
+    /// Store a constant value.
+    /// </summary>
     StoreConstant,
+
+    /// <summary>
+    /// Store a dynamic value.
+    /// </summary>
     StoreValue,
+
+    /// <summary>
+    /// Append a constant value.
+    /// </summary>
     AppendConstant,
+
+    /// <summary>
+    /// Append a dynamic value.
+    /// </summary>
     AppendValue,
+
+    /// <summary>
+    /// Represents a count flag (e.g., for counting occurrences).
+    /// </summary>
     CountFlag,
+
+    /// <summary>
+    /// Represents a help message action.
+    /// </summary>
     HelpMessage,
+
+    /// <summary>
+    /// Represents a version output action.
+    /// </summary>
     VersionOutput
 };
