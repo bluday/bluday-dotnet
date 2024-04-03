bacfd05846c1be441ff87478dcbe043517366dad
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-03 17:00:21 +0200

    chore: add xml documentation for OptionalArgument

--- src/BluDay.Net/BluDay.Net/CommandLine/OptionalArgument.cs
+++ src/BluDay.Net/BluDay.Net/CommandLine/OptionalArgument.cs
@@ -1,19 +1,39 @@
 namespace BluDay.Net.CommandLine;
 
+/// <summary>
+/// A descriptor for an optional command-line argument.
+/// </summary>
 public class OptionalArgument : Argument
 {
     private readonly ArgumentFlag? _longFlag;
 
     private readonly ArgumentFlag? _shortFlag;
 
+    /// <summary>
+    /// Gets either the short or the long flag, depending on flag descriptor provided at instantiation.
+    /// </summary>
     public ArgumentFlag PrimaryFlag => (_shortFlag ?? _longFlag)!.Value;
 
+    /// <summary>
+    /// Gets the long flag.
+    /// </summary>
     public ArgumentFlag? LongFlag => _longFlag;
 
+    /// <summary>
+    /// Gets the short flag.
+    /// </summary>
     public ArgumentFlag? ShortFlag => _shortFlag;
 
+    /// <summary>
+    /// <inheritdoc cref="IArgument.Name"/>
+    /// </summary>
     public required new string Name { get; init; }
 
+    /// <summary>
+    /// Initializes an new instance with a flags described in a <see cref="string"/> value.
+    /// </summary>
+    /// <param name="flagDescriptor">The flag descriptor.</param>
+    /// <exception cref="InvalidArgumentFlagLengthException"></exception>
     public OptionalArgument(string flagDescriptor)
     {
         ArgumentException.ThrowIfNullOrWhiteSpace(flagDescriptor);
