90c9f84d9c0f7a64744d89b77c3e63c8b9f9d525
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-03 16:00:27 +0200

    chore: add xml documentation for ArgumentStoreKind

--- src/BluDay.Net/BluDay.Net/CommandLine/ArgumentStoreKind.cs
+++ src/BluDay.Net/BluDay.Net/CommandLine/ArgumentStoreKind.cs
@@ -1,10 +1,32 @@
 namespace BluDay.Net.CommandLine;
 
+/// <summary>
+/// Represents different kinds of arguments in a store.
+/// </summary>
 public enum ArgumentStoreKind
 {
+    /// <summary>
+    /// Represents a boolean value.
+    /// </summary>
     Boolean,
+
+    /// <summary>
+    /// Represents a integer value.
+    /// </summary>
     Integer,
+
+    /// <summary>
+    /// Represents a floating-point value.
+    /// </summary>
     Point,
+
+    /// <summary>
+    /// Represents a string value.
+    /// </summary>
     String,
+
+    /// <summary>
+    /// Represents a character value.
+    /// </summary>
     Char
 };
