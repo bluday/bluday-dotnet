2259ef644d86a5ad9182dd98e7c51dc0c3f02783
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-03 17:03:15 +0200

    chore: add xml documentation for PositionalArgument

--- src/BluDay.Net/BluDay.Net/CommandLine/PositionalArgument.cs
+++ src/BluDay.Net/BluDay.Net/CommandLine/PositionalArgument.cs
@@ -1,9 +1,18 @@
 namespace BluDay.Net.CommandLine;
 
+/// <summary>
+/// A descriptor for positional command-line arguments.
+/// </summary>
 public class PositionalArgument : Argument
 {
+    /// <summary>
+    /// Gets the symbol identifier.
+    /// </summary>
     public string Symbol { get; }
 
+    /// <summary>
+    /// Initializes a new instance.
+    /// </summary>
     public PositionalArgument()
     {
         Name = nameof(PositionalArgument);
