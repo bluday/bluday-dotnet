7796daf1d0ff45c37675e8955c00f3885ce59a65
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-04 11:56:13 +0200

    feat: add changes from untracked ArgumentAttribute class

--- src/BluDay.Net/BluDay.Net/Common/Attributes/ArgumentAttribute.cs
+++ src/BluDay.Net/BluDay.Net/Common/Attributes/ArgumentAttribute.cs
@@ -1,6 +1,6 @@
 namespace BluDay.Net.Common.Attributes;
 
-public sealed class ArgumentAttribute : Attribute
+public abstract class ArgumentAttribute : Attribute
 {
     public string? TargetName { get; init; }
 }
