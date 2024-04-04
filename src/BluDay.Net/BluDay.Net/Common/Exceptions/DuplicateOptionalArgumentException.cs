44204d1eca85db5d5c1ebcdba5227955daf29b56
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-04 13:29:36 +0200

    feat: remove unneeded exception

--- src/BluDay.Net/BluDay.Net/Common/Exceptions/DuplicateOptionalArgumentException.cs
+++
@@ -1,7 +0,0 @@
-namespace BluDay.Net.Common.Exceptions;
-
-public sealed class DuplicateOptionalArgumentException : Exception
-{
-    public DuplicateOptionalArgumentException(OptionalArgument argument)
-        : base(ExceptionMessages.DUPLICATE_OPTIONAL_ARGUMENT.Format(argument)) { }
-}
