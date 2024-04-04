d241032c46e73f253bf462db244574c960f67944
BluDay <19403893+BluDay@users.noreply.github.com>
2024-04-04 13:00:29 +0200

    feat: add new Arguments class

---
+++ src/BluDay.Net/BluDay.Net/CommandLine/Arguments.cs
@@ -0,0 +1,39 @@
+﻿namespace BluDay.Net.CommandLine;
+
+/// <summary>
+/// Represents immutable command-line argument descriptors.
+/// </summary>
+public sealed class Arguments
+{
+    private readonly ImmutableList<OptionalArgument> _optionals;
+
+    private readonly ImmutableList<PositionalArgument> _positionals;
+
+    /// <summary>
+    /// Gets an immutable list of distinct optional argument descriptors.
+    /// </summary>
+    public IImmutableList<OptionalArgument> Optionals
+    {
+        get  => _optionals;
+        init => _optionals = value.Distinct().ToImmutableList();
+    }
+
+    /// <summary>
+    /// Gets an immutable list of distinct positional argument descriptors.
+    /// </summary>
+    public IImmutableList<PositionalArgument> Positionals
+    {
+        get  => _positionals;
+        init => _positionals = value.Distinct().ToImmutableList();
+    }
+
+    /// <summary>
+    /// Initializes a new instance with default values.
+    /// </summary>
+    public Arguments()
+    {
+        _optionals = ImmutableList<OptionalArgument>.Empty;
+
+        _positionals = ImmutableList<PositionalArgument>.Empty;
+    }
+}
--- src/BluDay.Net/BluDay.Net/CommandLine/ArgumentsParser{TArguments}.cs
+++ src/BluDay.Net/BluDay.Net/CommandLine/ArgumentsParser{TArguments}.cs
@@ -6,11 +6,9 @@
 /// <typeparam name="TArguments">The target type for argument mapping.</typeparam>
 public class ArgumentsParser<TArguments> where TArguments : new()
 {
+    private readonly Arguments _arguments;
+
     private readonly Type _resultType;
-
-    private readonly IImmutableList<OptionalArgument> _optionals;
-
-    private readonly IImmutableList<PositionalArgument> _positionals;
 
     /// <summary>
     /// Gets the type of object that <see cref="Parse(string[])"/> method returns.
@@ -20,42 +18,17 @@ public class ArgumentsParser<TArguments> where TArguments : new()
     /// <summary>
     /// Gets an immutable list of distinct optional argument descriptors.
     /// </summary>
-    public IImmutableList<OptionalArgument> OptionalArguments => _optionals;
+    public Arguments Arguments => _arguments;
 
     /// <summary>
-    /// Gets an immutable list of distinct positional argument descriptors.
+    /// Initializes a new instance and with pre-defined arguments.
     /// </summary>
-    public IImmutableList<PositionalArgument> PositionalArguments => _positionals;
-
-    /// <summary>
-    /// Initializes a new instance with default values.
-    /// </summary>
-    public ArgumentsParser() : this(null!, null!) { }
-
-    /// <summary>
-    /// Initializes a new instance with positional argument descriptors.
-    /// </summary>
-    /// <param name="positional">The postional argument.</param>
-    public ArgumentsParser(IEnumerable<PositionalArgument> positionals) : this(null!, positionals) { }
-
-    /// <summary>
-    /// Initializes a new instance with optional argument descriptors.
-    /// </summary>
-    /// <param name="optionals">An enumerable of optionals arguments.</param>
-    public ArgumentsParser(IEnumerable<OptionalArgument> optionals) : this(optionals, null!) { }
-
-    /// <summary>
-    /// Initializes a new instance with optional and positional arguments.
-    /// </summary>
-    /// <param name="optionals">An enumerable of optionals arguments.</param>
-    /// <param name="positional">An enumerable of positional arguments.</param>
-    public ArgumentsParser(IEnumerable<OptionalArgument> optionals, IEnumerable<PositionalArgument> positionals)
+    /// <param name="arguments">A descriptor of arguments.</param>
+    public ArgumentsParser(Arguments arguments)
     {
         _resultType = typeof(TArguments);
 
-        _optionals = optionals.Distinct().ToImmutableList();
-
-        _positionals = positionals.Distinct().ToImmutableList();
+        _arguments = arguments ?? new();
     }
 
     /// <summary>
