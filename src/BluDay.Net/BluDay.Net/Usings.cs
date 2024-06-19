656ac9ddd19e46a7cc75af8054f4c66333679c3b
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-19 23:38:30 +0200

    style: separate global usings into categorized groups

--- src/BluDay.Net/BluDay.Net/Usings.cs
+++ src/BluDay.Net/BluDay.Net/Usings.cs
@@ -1,16 +1,16 @@
 global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Messaging;
-global using BluDay.Net.Attributes;
-global using BluDay.Net.CommandLine;
+
+global using BluDay.Net.Domain.ViewModels;
 global using BluDay.Net.Exceptions;
 global using BluDay.Net.Extensions;
-global using BluDay.Net.Domain.ViewModels;
+global using BluDay.Net.Messaging;
 global using BluDay.Net.Resources;
 global using BluDay.Net.UI.Appearance;
 global using BluDay.Net.UI.Navigation;
 global using BluDay.Net.UI.WindowManagement;
+
 global using Microsoft.Extensions.DependencyInjection;
+
 global using System.Collections.Immutable;
-global using System.Diagnostics.CodeAnalysis;
 global using System.Reflection;
-global using System.Text;
