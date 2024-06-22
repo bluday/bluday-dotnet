f405d1bf40c49a87818b833b0f4229186a37dc36
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-23 01:15:46 +0200

    feat: remove property setter and add constructor to ViewModelAttribute

--- src/BluDay.Net/BluDay.Net/Attributes/ViewModelAttribute.cs
+++ src/BluDay.Net/BluDay.Net/Attributes/ViewModelAttribute.cs
@@ -2,5 +2,20 @@
 
 public sealed class ViewModelAttribute : Attribute
 {
-    public Type ViewModelType { get; set; } = null!;
+    /// <summary>
+    /// Gets the targeted viewmodel type.
+    /// </summary>
+    public Type ViewModelType { get; }
+
+    /// <summary>
+    /// Initializes a new instance with the provided viewmodel type.
+    /// </summary>
+    /// <param name="viewModelType">The targeted viewmodel type.</param>
+    /// <exception cref="ArgumentNullException">If the viewmodel type is null.</exception>
+    public ViewModelAttribute(Type viewModelType)
+    {
+        ArgumentNullException.ThrowIfNull(viewModelType);
+
+        ViewModelType = viewModelType;
+    }
 }
