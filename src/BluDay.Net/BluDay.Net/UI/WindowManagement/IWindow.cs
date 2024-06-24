4f899925143110c088817e723eb50d2abf81662a
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-24 23:30:12 +0200

    feat: add members to IWindow

--- src/BluDay.Net/BluDay.Net/UI/WindowManagement/IWindow.cs
+++ src/BluDay.Net/BluDay.Net/UI/WindowManagement/IWindow.cs
@@ -1,3 +1,17 @@
 namespace BluDay.Net.UI.WindowManagement;
 
-public interface IWindow { }
+/// <summary>
+/// Represents the control of a window.
+/// </summary>
+public interface IWindow
+{
+    /// <summary>
+    /// Activates and shows the current window.
+    /// </summary>
+    void Activate();
+
+    /// <summary>
+    /// Closes the current window.
+    /// </summary>
+    void Close();
+}
