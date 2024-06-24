1b19f3dc07fe159ccd9523efbfb0cac01525f826
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-24 23:30:32 +0200

    feat: add new INavigableWindow type

---
+++ src/BluDay.Net/BluDay.Net/UI/WindowManagement/INavigableWindow.cs
@@ -0,0 +1,12 @@
+namespace BluDay.Net.UI.WindowManagement;
+
+/// <summary>
+/// Represents the control of a window.
+/// </summary>
+public interface INavigableWindow : IWindow
+{
+    /// <summary>
+    /// Gets the navigator instance for handling view navigation within the window.
+    /// </summary>
+    INavigator Navigator { get; }
+}
