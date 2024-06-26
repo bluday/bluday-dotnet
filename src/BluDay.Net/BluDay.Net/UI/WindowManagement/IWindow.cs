15806927cccdac6ee23c2c4879aabf3373e09c43
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-26 22:45:46 +0200

    feat: add IWindow.Resize method

--- src/BluDay.Net/BluDay.Net/UI/WindowManagement/IWindow.cs
+++ src/BluDay.Net/BluDay.Net/UI/WindowManagement/IWindow.cs
@@ -14,4 +14,11 @@ public interface IWindow
     /// Closes the current window.
     /// </summary>
     void Close();
+
+    /// <summary>
+    /// Resizes the current window to the provided width and height values.
+    /// </summary>
+    /// <param name="width">The width of the window, in pixels.</param>
+    /// <param name="height">The height of the window, in pixels</param>
+    void Resize(int width, int height);
 }
