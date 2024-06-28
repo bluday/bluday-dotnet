58e27121c5b1ed9d8d4b1c09831d34c6cd9a580a
BluDay <19403893+BluDay@users.noreply.github.com>
2024-06-29 01:21:41 +0200

    docs: document IViewNavigator

--- src/BluDay.Net/BluDay.Net/UI/Navigation/IViewNavigator.cs
+++ src/BluDay.Net/BluDay.Net/UI/Navigation/IViewNavigator.cs
@@ -1,22 +1,67 @@
 namespace BluDay.Net.UI.Navigation;
 
+/// <summary>
+/// Represents a view navigator for managing navigation between views.
+/// </summary>
 public interface IViewNavigator
 {
+    /// <summary>
+    /// Gets a value indicating whether navigation to the previous view is possible.
+    /// </summary>
     bool CanGoBack { get; }
 
+    /// <summary>
+    /// Gets a value indicating whether navigation to the next view is possible.
+    /// </summary>
     bool CanGoForward { get; }
 
+    /// <summary>
+    /// Gets or sets the maximum size of the view cache.
+    /// </summary>
     int MaxCacheSize { get; set; }
 
+    /// <summary>
+    /// Gets the unique identifier associated with the window.
+    /// </summary>
     Guid WindowId { get; }
 
+    /// <summary>
+    /// Gets an enumerable collection of view types for all displayed views.
+    /// </summary>
     IEnumerable<Type> Views { get; }
 
+    /// <summary>
+    /// Removes the top view from the navigation stack.
+    /// </summary>
+    /// <returns>
+    /// <c>true</c> if successful, <c>false</c> if the stack is empty.
+    /// </returns>
     bool Pop();
 
+    /// <summary>
+    /// Pushes a new view onto the navigation stack based on the specific view model type.
+    /// </summary>
+    /// <param name="viewModelType">
+    /// The type of the view model associated with the view.
+    /// </param>
+    /// <returns>
+    /// <c>true</c> if successful.
+    /// </returns>
     bool Push(Type viewModelType);
 
+    /// <summary>
+    /// Pushes a new view onto the navigation stack based on the specific view model type.
+    /// </summary>
+    /// <typeparam name="TViewModel">
+    /// The type of the view model associated with the view.
+    /// </typeparam>
+    /// <returns>
+    /// <c>true</c> if successful.
+    /// </returns>
     bool Push<TViewModel>() where TViewModel : IViewModel;
 
+    /// <summary>
+    /// Resets the view navigation stack.
+    /// </summary>
     void Reset();
 }
