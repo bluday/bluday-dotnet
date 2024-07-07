9b527394b8fc7ca44397940c2850aa40bcf1c8f9
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-08 01:02:29 +0200

    feat: rename test method and add additional method

--- src/BluDay.Net.Tests/BluDay.Net.Tests/Unit/AppActivationServiceTests.cs
+++ src/BluDay.Net.Tests/BluDay.Net.Tests/Unit/AppActivationServiceTests.cs
@@ -4,13 +4,26 @@
 public sealed class AppActivationServiceTests
 {
     [TestMethod]
-    public void GetIsActivated_AfterActivation_AndExpectTrue()
+    public void GetIsActivated_AfterExplicitActivationCall_AndExpectTrue()
     {
         // Arrange
         AppActivationService service = new(WeakReferenceMessenger.Default);
 
         // Act
         service.Activate();
+
+        // Assert
+        Assert.IsTrue(service.IsActivated);
+    }
+
+    [TestMethod]
+    public void GetIsActivated_AfterSentActivationRequestMessage_AndExpectTrue()
+    {
+        // Arrange
+        AppActivationService service = new(WeakReferenceMessenger.Default);
+
+        // Act
+        WeakReferenceMessenger.Default.Send<AppActivationRequestMessage>();
 
         // Assert
         Assert.IsTrue(service.IsActivated);
--- src/BluDay.Net.Tests/BluDay.Net.Tests/Usings.cs
+++ src/BluDay.Net.Tests/BluDay.Net.Tests/Usings.cs
@@ -1,3 +1,4 @@
 global using CommunityToolkit.Mvvm.Messaging;
 
+global using BluDay.Net.Messaging;
 global using BluDay.Net.Services;
