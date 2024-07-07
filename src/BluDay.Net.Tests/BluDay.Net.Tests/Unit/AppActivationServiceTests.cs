87a528f539eac7c5301477917d6053a9e7f1213b
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-08 01:24:23 +0200

    refactor: add field in order to violate DRY less

--- src/BluDay.Net.Tests/BluDay.Net.Tests/Unit/AppActivationServiceTests.cs
+++ src/BluDay.Net.Tests/BluDay.Net.Tests/Unit/AppActivationServiceTests.cs
@@ -3,11 +3,13 @@
 [TestClass]
 public sealed class AppActivationServiceTests
 {
+    private readonly WeakReferenceMessenger _messenger = WeakReferenceMessenger.Default;
+
     [TestMethod]
     public void GetIsActivated_AfterExplicitActivationCall_AndExpectTrue()
     {
         // Arrange
-        AppActivationService service = new(WeakReferenceMessenger.Default);
+        AppActivationService service = new(_messenger);
 
         // Act
         service.Activate();
@@ -17,15 +19,41 @@ public sealed class AppActivationServiceTests
     }
 
     [TestMethod]
+    public void GetIsActivated_AfterExplicitDeactivationCall_AndExpectTrue()
+    {
+        // Arrange
+        AppActivationService service = new(_messenger);
+
+        // Act
+        service.Deactivate();
+
+        // Assert
+        Assert.IsFalse(service.IsActivated);
+    }
+
+    [TestMethod]
     public void GetIsActivated_AfterSentActivationRequestMessage_AndExpectTrue()
     {
         // Arrange
-        AppActivationService service = new(WeakReferenceMessenger.Default);
+        AppActivationService service = new(_messenger);
 
         // Act
-        WeakReferenceMessenger.Default.Send<AppActivationRequestMessage>();
+        _messenger.Send<AppActivationRequestMessage>();
 
         // Assert
         Assert.IsTrue(service.IsActivated);
+    }
+
+    [TestMethod]
+    public void GetIsActivated_AfterSentDeactivationRequestMessage_AndExpectTrue()
+    {
+        // Arrange
+        AppActivationService service = new(_messenger);
+
+        // Act
+        _messenger.Send<AppDeactivationRequestMessage>();
+
+        // Assert
+        Assert.IsFalse(service.IsActivated);
     }
 }
