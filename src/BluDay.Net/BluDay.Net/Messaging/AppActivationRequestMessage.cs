4d1ecb639d783323d3f5cbb1b2f45f6249588ea7
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-08 00:18:26 +0200

    feat: add new app activation request message classes

---
+++ src/BluDay.Net/BluDay.Net/Messaging/AppActivationRequestMessage.cs
@@ -0,0 +1,6 @@
+﻿namespace BluDay.Net.Messaging;
+
+/// <summary>
+/// Represents a message to activate an app.
+/// </summary>
+public sealed class AppActivationRequestMessage { }
---
+++ src/BluDay.Net/BluDay.Net/Messaging/AppDeactivationRequestMessage.cs
@@ -0,0 +1,6 @@
+﻿namespace BluDay.Net.Messaging;
+
+/// <summary>
+/// Represents a message to deactivate an active app.
+/// </summary>
+public sealed class AppDeactivationRequestMessage { }
