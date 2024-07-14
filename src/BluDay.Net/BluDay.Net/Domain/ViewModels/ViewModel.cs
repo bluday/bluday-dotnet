a0a4ff4bb87d137af5069e4934c0753c70bb54bc
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-14 11:24:13 +0200

    feat: add messaging functionality to the base view model class

--- src/BluDay.Net/BluDay.Net/Domain/ViewModels/ViewModel.cs
+++ src/BluDay.Net/BluDay.Net/Domain/ViewModels/ViewModel.cs
@@ -1,3 +1,32 @@
 ﻿namespace BluDay.Net.Domain.ViewModels;
 
-public abstract class ViewModel : ObservableObject, IViewModel { }
+/// <summary>
+/// Represents the base of a view model class.
+/// </summary>
+public abstract class ViewModel : ObservableObject, IViewModel
+{
+    protected readonly WeakReferenceMessenger _messenger;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ViewModel"/> class.
+    /// </summary>
+    /// <param name="messenger">
+    /// The <see cref="WeakReference"/>-based messenger instance.
+    /// </param>
+    public ViewModel(WeakReferenceMessenger messenger)
+    {
+        ArgumentNullException.ThrowIfNull(messenger);
+
+        _messenger = messenger;
+    }
+
+    /// <summary>
+    /// Registers recipients of specified message types.
+    /// </summary>
+    protected virtual void Subscribe() { }
+
+    /// <summary>
+    /// Unregisters recipients of specified message types.
+    /// </summary>
+    protected virtual void Unsubscribe() { }
+}
