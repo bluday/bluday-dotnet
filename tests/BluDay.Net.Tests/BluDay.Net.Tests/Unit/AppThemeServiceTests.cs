8b3070f6b2ecffddf5ea0e9087f19a8bc47ab5f5
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-15 19:35:23 +0200

    feat: add unit tests for AppThemeService

---
+++ tests/BluDay.Net.Tests/BluDay.Net.Tests/Unit/AppThemeServiceTests.cs
@@ -0,0 +1,44 @@
+﻿namespace BluDay.Net.Tests.Unit;
+
+[TestClass]
+public sealed class AppThemeServiceTests
+{
+    private readonly WeakReferenceMessenger _messenger = WeakReferenceMessenger.Default;
+
+    [TestMethod]
+    public void SetAndGetCurrentThemeExplicitly()
+    {
+        // Arrange.
+        AppThemeService service = new(_messenger);
+
+        // Act.
+        service.CurrentTheme = AppTheme.Dark;
+
+        // Assert.
+        Assert.IsTrue(service.CurrentTheme is AppTheme.Dark);
+    }
+
+    [TestMethod]
+    public void SetCurrentThemeAndReceiveUpdateMessage()
+    {
+        // Arrange.
+        AppThemeService service = new(_messenger);
+
+        AppTheme currentTheme = default;
+
+        void MessageHandler(object sender, AppThemeChangedMessage e)
+        {
+            currentTheme = service.CurrentTheme;
+
+            _messenger.Unregister<AppThemeChangedMessage>(this);
+        }
+
+        _messenger.Register<AppThemeChangedMessage>(this, MessageHandler);
+
+        // Act.
+        service.CurrentTheme = AppTheme.Dark;
+
+        // Assert.
+        Assert.IsTrue(currentTheme is AppTheme.Dark);
+    }
+}
--- tests/BluDay.Net.Tests/BluDay.Net.Tests/Usings.cs
+++ tests/BluDay.Net.Tests/BluDay.Net.Tests/Usings.cs
@@ -2,3 +2,4 @@ global using CommunityToolkit.Mvvm.Messaging;
 
 global using BluDay.Net.Messaging;
 global using BluDay.Net.Services;
+global using BluDay.Net.UI.Appearance;
