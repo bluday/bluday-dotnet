db37ab3d821cae19f13e5a69a26ac903dc973bc4
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-16 15:34:47 +0200

    chore: reorder global using statements

--- src/BluDay.Net/BluDay.Net/Usings.cs
+++ src/BluDay.Net/BluDay.Net/Usings.cs
@@ -3,10 +3,9 @@ global using CommunityToolkit.Mvvm.Messaging;
 global using CommunityToolkit.Mvvm.Messaging.Messages;
 
 global using BluDay.Net.Abstractions.Services;
-global using BluDay.Net.Common;
+global using BluDay.Net.Abstractions.ViewModels;
 global using BluDay.Net.Common.Exceptions;
 global using BluDay.Net.Common.Extensions;
-global using BluDay.Net.Abstractions.ViewModels;
 global using BluDay.Net.Messaging;
 global using BluDay.Net.Resources;
 global using BluDay.Net.UI.Appearance;
