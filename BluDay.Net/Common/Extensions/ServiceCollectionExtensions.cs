b30b9f6ef4a8266d36e3dbe97b95e969e1636c97
BluDay <19403893+BluDay@users.noreply.github.com>
2024-08-12 14:14:26 +0200

    feat: add implementation to `ServiceCollectionExtensions.AddViewModels()`

--- BluDay.Net/Common/Extensions/ServiceCollectionExtensions.cs
+++ BluDay.Net/Common/Extensions/ServiceCollectionExtensions.cs
@@ -6,17 +6,20 @@ namespace BluDay.Net.Extensions;
 public static class ServiceCollectionExtensions
 {
     /// <summary>
-    /// 
+    /// Registers all view model types from the provided <paramref name="assembly"/>.
     /// </summary>
     /// <param name="source">
-    /// 
+    /// The targeted service collection instance.
     /// </param>
     /// <returns>
-    /// 
+    /// The service collection instance.
     /// </returns>
     public static IServiceCollection AddViewModels(this IServiceCollection source)
     {
-        // ( 0 _ o )
+        foreach (Type viewModelType in typeof(ViewModel).GetImplementationTypes())
+        {
+            source.AddTransient(viewModelType);
+        }
 
         return source;
     }
