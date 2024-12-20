d973ac1ece46a4d320ec5709977e70b19780945c
BluDay <19403893+BluDay@users.noreply.github.com>
2024-12-21 00:21:45 +0100

    feat(winui3): add method extension class for `ServiceCollection`

---
+++ BluDay.Net.WinUI3/Common/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace BluDay.Net.WinUI3.Common.Extensions;
+
+/// <summary>
+/// A utility class with method extensions for the <see cref="ServiceCollection"/> type.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Registers all views of type <see cref="UserControl"/>.
+    /// </summary>
+    /// <param name="source">
+    /// The service collection.
+    /// </param>
+    /// <returns>
+    /// The service collection to chained calls.
+    /// </returns>
+    public static IServiceCollection AddViews(this IServiceCollection source)
+    {
+        Assembly assembly = Assembly.GetCallingAssembly();
+
+        foreach (Type viewType in typeof(UserControl).GetImplementationTypes(assembly))
+        {
+            if (!viewType.Name[-4..].Equals("View"))
+            {
+                continue;
+            }
+
+            source.AddTransient(viewType);
+        }
+
+        return source;
+    }
+}
--- BluDay.Net.WinUI3/Usings.cs
+++ BluDay.Net.WinUI3/Usings.cs
@@ -1,6 +1,7 @@
 ﻿global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Messaging;
 
+global using BluDay.Net.Extensions;
 global using BluDay.Net.Services;
 global using BluDay.Net.ViewModels;
 global using BluDay.Net.UI.Navigation;
@@ -16,9 +17,11 @@ global using Microsoft.UI;
 global using Microsoft.UI.Dispatching;
 global using Microsoft.UI.Windowing;
 global using Microsoft.UI.Xaml;
+global using Microsoft.UI.Xaml.Controls;
 
 global using System;
 global using System.Drawing;
+global using System.Reflection;
 global using System.Runtime.InteropServices;
 global using System.Threading;
 
