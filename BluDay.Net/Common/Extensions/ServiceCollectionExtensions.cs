06b114afe4ecc498cb1f0a61cfac720027fcf420
BluDay <19403893+bluday@users.noreply.github.com>
2025-01-21 23:40:38 +0100

    feat: add new `ServiceCollectionExtensions.Add` extension method

--- BluDay.Net/Common/Extensions/ServiceCollectionExtensions.cs
+++ BluDay.Net/Common/Extensions/ServiceCollectionExtensions.cs
@@ -6,10 +6,29 @@ namespace BluDay.Net.Extensions;
 public static class ServiceCollectionExtensions
 {
     /// <summary>
+    /// Registers multiple service descriptors to a service collection using the provided callback.
+    /// </summary>
+    /// <param name="source">
+    /// The targeted instance.
+    /// </param>
+    /// <param name="factory">
+    /// The service descriptor configuring function.
+    /// </param>
+    /// <returns>
+    /// The service collection instance.
+    /// </returns>
+    public static IServiceCollection Add(this IServiceCollection source, Action<IServiceCollection> factory)
+    {
+        factory(source);
+
+        return source;
+    }
+
+    /// <summary>
     /// Registers all view model types from the calling assembly.
     /// </summary>
     /// <param name="source">
-    /// The targeted service collection instance.
+    /// The targeted instance.
     /// </param>
     /// <returns>
     /// The service collection instance.
