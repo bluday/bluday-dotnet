250843d273fae2f9c5962a37d1a265a8bd500f34
BluDay <19403893+bluday@users.noreply.github.com>
2025-02-11 08:08:06 +0100

    docs: add docstrings for `AppTheme` enum

--- BluDay.Net/UI/Appearance/AppTheme.cs
+++ BluDay.Net/UI/Appearance/AppTheme.cs
@@ -1,3 +1,22 @@
 namespace BluDay.Net.UI.Appearance;
 
-public enum AppTheme { System, Light, Dark };
+/// <summary>
+/// Represents different kinds of application themes.
+/// </summary>
+public enum AppTheme
+{
+    /// <summary>
+    /// Represents the system default theme.
+    /// </summary>
+    System,
+
+    /// <summary>
+    /// Represents the light theme.
+    /// </summary>
+    Light,
+
+    /// <summary>
+    /// Represents the dark theme.
+    /// </summary>
+    Dark
+};
