c93b3499c360237001362d922c908fa3e130b9ce
BluDay <19403893+bluday@users.noreply.github.com>
2025-03-13 14:25:04 +0100

    chore(winui): remove unused global using statement

--- BluDay.Net.WinUI3/Usings.cs
+++ BluDay.Net.WinUI3/Usings.cs
@@ -1,7 +1,6 @@
 ﻿global using CommunityToolkit.Mvvm.ComponentModel;
 global using CommunityToolkit.Mvvm.Messaging;
 
-global using BluDay.Net.Abstractions.Handlers;
 global using BluDay.Net.Common.Extensions;
 global using BluDay.Net.Services;
 global using BluDay.Net.ViewModels;
@@ -10,7 +9,6 @@ global using BluDay.Net.WinUI3.Common.Extensions;
 global using BluDay.Net.WinUI3.ComponentModel;
 
 global using Microsoft.Extensions.DependencyInjection;
-
 global using Microsoft.UI;
 global using Microsoft.UI.Dispatching;
 global using Microsoft.UI.Windowing;
