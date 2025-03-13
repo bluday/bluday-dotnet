9fe6735458e06369548c6f1afb466287f692c022
BluDay <19403893+bluday@users.noreply.github.com>
2025-03-13 12:37:15 +0100

    feat(core): add new `Handlers` namespace and app activation handler class

---
+++ BluDay.Net/Abstractions/Handlers/IAppActivationHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace BluDay.Net.Abstractions.Handlers;
+
+/// <summary>
+/// Defines a contract for handling app activation events specific to a platform.
+/// </summary>
+public interface IAppActivationHandler
+{
+    /// <summary>
+    /// Activates the application asynchronously.
+    /// </summary>
+    /// <returns>
+    /// A task that represents the asynchronous activation operation.
+    /// </returns>
+    Task ActivateAsync();
+}
