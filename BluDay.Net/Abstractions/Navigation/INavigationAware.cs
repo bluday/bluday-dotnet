986f2926c9eb9c97ce84a8153d23f3cc4cdacaba
BluDay <19403893+bluday@users.noreply.github.com>
2025-03-26 17:05:50 +0100

    refactor(core): remove generic `INavigationAware` interface

--- BluDay.Net/Abstractions/Navigation/INavigationAware.cs
+++ BluDay.Net/Abstractions/Navigation/INavigationAware.cs
@@ -1,4 +1,32 @@
 ﻿namespace BluDay.Net.Abstractions.Navigation;
 
-/// <inheritdoc/>
-public interface INavigationAware : INavigationAware<object> { }
+/// <summary>
+/// Defines an interface for handling navigation events and their lifecycle stages with
+/// asynchronous operations.
+/// </summary>
+public interface INavigationAware
+{
+    /// <summary>
+    /// Asynchronously called after the object has been navigated away from. Typically
+    /// used for cleanup or releasing resources associated with the view or viewmodel.
+    /// </summary>
+    Task OnNavigatedFromAsync();
+
+    /// <summary>
+    /// Asynchronously called after the object has been navigated to, providing optional 
+    /// navigation parameters. Typically used for setting up the state or initializing
+    /// data.
+    /// </summary>
+    Task OnNavigatedToAsync(object? parameter);
+
+    /// <summary>
+    /// Asynchronously called before the object is navigated away from. Allows for
+    /// pre-navigation tasks such as validation, prompting the user, or performing
+    /// cleanup operations.
+    /// </summary>
+    /// <returns>
+    /// A task representing the asynchronous operation. Returns <c>true</c> if
+    /// navigation can proceed; otherwise, <c>false</c>.
+    /// </returns>
+    Task<bool> OnNavigatingFromAsync();
+}
