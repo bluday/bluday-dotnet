d0b5bf037818a34f0ee1b68836f1ed877e5561bd
BluDay <19403893+bluday@users.noreply.github.com>
2025-03-26 16:54:00 +0100

    refactor(core): make methods in `INavigationAware<TParameter>` asynchronous

--- BluDay.Net/Abstractions/Navigation/INavigationAware{TParameter}.cs
+++ BluDay.Net/Abstractions/Navigation/INavigationAware{TParameter}.cs
@@ -1,34 +1,39 @@
 ﻿namespace BluDay.Net.Abstractions.Navigation;
 
 /// <summary>
-/// Defines an interface for handling navigation events and their lifecycle stages.
+/// Defines an interface for handling navigation events and their lifecycle stages with
+/// asynchronous operations.
 /// </summary>
 /// <typeparam name="TParameter">
-/// The type of parameter data passed during navigation.
+/// The type of parameter data passed during navigation, used for initialization or
+/// contextual information.
 /// </typeparam>
 public interface INavigationAware<TParameter> where TParameter : class
 {
     /// <summary>
-    /// Called after the object has been navigated away from. Typically used for
-    /// cleanup or finalization tasks.
+    /// Asynchronously called after the object has been navigated away from. Typically
+    /// used for cleanup or releasing resources associated with the view or viewmodel.
     /// </summary>
-    void OnNavigatedFrom();
+    Task OnNavigatedFromAsync();
 
     /// <summary>
-    /// Called before the object is navigated away from. Allows for validation, user
-    /// prompts, or cancellation of navigation.
-    /// </summary>
-    /// <returns>
-    /// <c>true</c> if navigation can proceed; otherwise, <c>false</c>.
-    /// </returns>
-    bool OnNavigatingFrom();
-
-    /// <summary>
-    /// Called after the object has been navigated to, with optional navigation
-    /// parameters provided. Typically used for initialization or setting up state.
+    /// Asynchronously called after the object has been navigated to, providing optional 
+    /// navigation parameters. Typically used for setting up the state or initializing
+    /// data.
     /// </summary>
     /// <param name="parameter">
     /// An optional parameter containing data passed during navigation.
     /// </param>
-    void OnNavigatedTo(TParameter? parameter);
+    Task OnNavigatedToAsync(TParameter? parameter);
+
+    /// <summary>
+    /// Asynchronously called before the object is navigated away from. Allows for
+    /// pre-navigation tasks such as validation, prompting the user, or performing
+    /// cleanup operations.
+    /// </summary>
+    /// <returns>
+    /// A task representing the asynchronous operation. Returns <c>true</c> if
+    /// navigation can proceed; otherwise, <c>false</c>.
+    /// </returns>
+    Task<bool> OnNavigatingFromAsync();
 }
