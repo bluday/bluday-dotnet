4e857d4f831d057d88bccfaa73fa80bb6f391896
BluDay <19403893+bluday@users.noreply.github.com>
2025-03-27 15:09:16 +0100

    feat(core): add new `INavigationHost` interface

---
+++ BluDay.Net/Abstractions/Navigation/INavigationHost.cs
@@ -0,0 +1,25 @@
+﻿namespace BluDay.Net.Abstractions.Navigation;
+
+/// <summary>
+/// Represents a navigation host responsible for managing navigation between child views
+/// or viewmodels in the application's navigation hierarchy.
+/// </summary>
+public interface INavigationHost
+{
+    /// <summary>
+    /// Gets the current active view being managed by the navigation host.
+    /// </summary>
+    INavigationAware? CurrentView { get; }
+
+    /// <summary>
+    /// Asynchronously navigates to a specified view, passing an optional view parameter.
+    /// </summary>
+    /// <param name="viewType">
+    /// The type of the navigation-aware view to navigate to.
+    /// </typeparam>
+    /// <param name="parameter">
+    /// An optional parameter containing data to be passed to the target view during
+    /// navigation.
+    /// </param>
+    Task NavigateAsync(Type viewType, object? parameter);
+}
