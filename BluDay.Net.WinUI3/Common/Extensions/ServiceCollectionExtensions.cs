7ca89711f26fc1b1b7e71d8e0b9fc0ef2ceed70c
BluDay <19403893+bluday@users.noreply.github.com>
2025-04-14 11:34:29 +0200

    feat(winui): add new `ServiceCollectionExtensions.AddWindows` method extension

--- BluDay.Net.WinUI3/Common/Extensions/ServiceCollectionExtensions.cs
+++ BluDay.Net.WinUI3/Common/Extensions/ServiceCollectionExtensions.cs
@@ -15,4 +15,33 @@ public static class ServiceCollectionExtensions
 
         return source.AddViews<UserControl>(Assembly.GetCallingAssembly());
     }
+
+    /// <summary>
+    /// Registers all derived window control types from the calling assembly.
+    /// </summary>
+    /// <param name="source">
+    /// The targeted instance.
+    /// </param>
+    /// <returns>
+    /// The service collection instance.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="source"/> is null.
+    /// </exception>
+    public static IServiceCollection AddWindows(this IServiceCollection source)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        IEnumerable<Type> windowTypes = Assembly
+            .GetCallingAssembly()
+            .GetTypes()
+            .Where(type => type.Name.Contains(nameof(Window)));
+
+        foreach (var windowType in windowTypes)
+        {
+            source.AddTransient(windowType);
+        }
+
+        return source;
+    }
 }
