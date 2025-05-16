5ed8f4fb8c013cbf98da49b12377b0759b78116c
BluDay <19403893+bluday@users.noreply.github.com>
2025-05-16 11:44:39 +0200

    feat: remove properties from `IBluWindow`

--- BluDay.Net/Abstractions/UI/Windowing/IBluWindow.cs
+++ BluDay.Net/Abstractions/UI/Windowing/IBluWindow.cs
@@ -6,11 +6,6 @@
 public interface IBluWindow
 {
     /// <summary>
-    /// Gets the id of the window.
-    /// </summary>
-    ulong Id { get; }
-
-    /// <summary>
     /// Activates and shows the current window.
     /// </summary>
     void Activate();
@@ -19,15 +14,4 @@ public interface IBluWindow
     /// Closes the current window.
     /// </summary>
     void Close();
-
-    /// <summary>
-    /// Resizes the window using the provided width and height values.
-    /// </summary>
-    /// <param name="width">
-    /// The width of the window, in pixels.
-    /// </param>
-    /// <param name="height">
-    /// The height of the window, in pixels
-    /// </param>
-    void Resize(int width, int height);
 }
