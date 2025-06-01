7a750cf55a8e6ca394bff8b8c6d8fdad8fd29fca
BluDay <19403893+bluday@users.noreply.github.com>
2025-06-01 12:37:31 +0200

    refactor(core): replace blu window type with more modular types

---
+++ BluDay.Net/Abstractions/UI/Windowing/IActivatableWindow.cs
@@ -0,0 +1,12 @@
+﻿namespace BluDay.Net.UI.Windowing;
+
+/// <summary>
+/// Represents a window that can be activated.
+/// </summary>
+public interface IActivatableWindow
+{
+    /// <summary>
+    /// Activates the window.
+    /// </summary>
+    void Activate();
+}
---
+++ BluDay.Net/Abstractions/UI/Windowing/IClosableWindow.cs
@@ -0,0 +1,12 @@
+﻿namespace BluDay.Net.UI.Windowing;
+
+/// <summary>
+/// Represents a window that can be closed.
+/// </summary>
+public interface IClosableWindow
+{
+    /// <summary>
+    /// Closes the window.
+    /// </summary>
+    void Close();
+}
