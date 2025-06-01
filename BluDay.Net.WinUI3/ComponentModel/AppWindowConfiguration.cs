8d111fd8252d0f21e5f576f4967575311fc355d5
BluDay <19403893+bluday@users.noreply.github.com>
2025-06-01 12:38:41 +0200

    feat(winui): add new configuration type for `AppWindow` instances

---
+++ BluDay.Net.WinUI3/ComponentModel/AppWindowConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace BluDay.Net.WinUI3.ComponentModel;
+
+/// <summary>
+/// Represents initial configuration for an <see cref="AppWindow"/> instance.
+/// </summary>
+public sealed class AppWindowConfiguration
+{
+    /// <summary>
+    /// Gets the icon path for the title bar.
+    /// </summary>
+    public string? IconPath { get; init; }
+
+    /// <inheritdoc cref="AppWindow.Size"/>
+    public SizeInt32? Size { get; init; }
+
+    /// <inheritdoc cref="AppWindow.Title"/>
+    public string? Title { get; init; }
+}
