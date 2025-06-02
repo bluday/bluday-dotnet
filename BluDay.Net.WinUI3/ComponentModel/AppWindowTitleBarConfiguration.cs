5a053dcb4c6ac4c570de196887c2326f4a3d564d
BluDay <19403893+bluday@users.noreply.github.com>
2025-06-02 10:53:26 +0200

    feat(winui): add new app window title bar configuration type

---
+++ BluDay.Net.WinUI3/ComponentModel/AppWindowTitleBarConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace BluDay.Net.WinUI3.ComponentModel;
+
+/// <summary>
+/// Represents initial configuration for an <see cref="AppWindowTitleBar"/> instance.
+/// </summary>
+public sealed class AppWindowTitleBarConfiguration
+{
+    /// <inheritdoc cref="AppWindowTitleBar.PreferredHeightOption"/>
+    public TitleBarHeightOption? PreferredHeightOption { get; init; }
+}
