2f39314aab4173ffc65752c2c32bf2c523a2ad1c
BluDay <19403893+bluday@users.noreply.github.com>
2025-06-18 01:17:36 +0200

    chore: clean up test projects

--- tests/BluDay.Net.WinUI3.Tests/UnitTests.cs
+++ tests/BluDay.Net.WinUI3.Tests/UnitTests.cs
@@ -1,27 +1,22 @@
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting.AppContainer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace BluDay.Net.WinUI3.Tests
+namespace BluDay.Net.WinUI3.Tests;
+
+[TestClass]
+public partial class UnitTest1
 {
-    [TestClass]
-    public partial class UnitTest1
+    [TestMethod]
+    public void TestMethod1()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            Assert.AreEqual(0, 0);
-        }
+        Assert.AreEqual(0, 0);
+    }
 
-        // Use the UITestMethod attribute for tests that need to run on the UI thread.
-        [UITestMethod]
-        public void TestMethod2()
-        {
-            var grid = new Grid();
-            Assert.AreEqual(0, grid.MinWidth);
-        }
+    // Use the UITestMethod attribute for tests that need to run on the UI thread.
+    [UITestMethod]
+    public void TestMethod2()
+    {
+        Assert.AreEqual(0, new Grid().MinWidth);
     }
 }
