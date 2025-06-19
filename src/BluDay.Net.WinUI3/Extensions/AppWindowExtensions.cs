724926d37a3a5309622288374929d4527e9c3d57
BluDay <19403893+bluday@users.noreply.github.com>
2025-06-19 14:55:08 +0200

    feat: method extensions for retrieving `DisplayArea`

--- src/BluDay.Net.WinUI3/Extensions/AppWindowExtensions.cs
+++ src/BluDay.Net.WinUI3/Extensions/AppWindowExtensions.cs
@@ -6,6 +6,18 @@ namespace BluDay.Net.WinUI3.Extensions;
 
 public static class AppWindowExtensions
 {
+    public static DisplayArea GetDisplayArea(this AppWindow source)
+    {
+        return source.GetDisplayArea(DisplayAreaFallback.Nearest);
+    }
+
+    public static DisplayArea GetDisplayArea(this AppWindow source, DisplayAreaFallback displayAreaFallback)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        return DisplayArea.GetFromWindowId(source.Id, displayAreaFallback);
+    }
+
     public static void Resize(this AppWindow source, int width, int height)
     {
         source.Resize(width, height, scaleFactor: null);
