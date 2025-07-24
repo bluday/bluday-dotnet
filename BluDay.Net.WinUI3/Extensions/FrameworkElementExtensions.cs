3b18bc9305723fef047fc031f5c77e5108dfd44c
BluDay <19403893+bluday@users.noreply.github.com>
2025-07-24 14:31:16 +0200

    docs: add docstrings to `FrameworkElementExtensions`

--- BluDay.Net.WinUI3/Extensions/FrameworkElementExtensions.cs
+++ BluDay.Net.WinUI3/Extensions/FrameworkElementExtensions.cs
@@ -5,17 +5,34 @@ using Windows.Graphics;
 
 namespace BluDay.Net.WinUI3.Extensions;
 
+/// <summary>
+/// Provides extension methods for <see cref="FrameworkElement"/> instances.
+/// </summary>
 public static class FrameworkElementExtensions
 {
+    /// <summary>
+    /// Computes the bounding box of the element in screen coordinates and returns it as an integer
+    /// rectangle, optionally scaled by the specified factor.
+    /// </summary>
+    /// <param name="source">
+    /// The <see cref="FrameworkElement"/> whose bounding box is to be calculated.
+    /// </param>
+    /// <param name="scale">
+    /// The scale factor to apply to the resulting dimensions. If negative, a default of 1 is used.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="source"/> is <c>null</c>.
+    /// </exception>
+    /// <returns>
+    /// A <see cref="RectInt32"/> representing the scaled bounding box of the element.
+    /// </returns>
     public static RectInt32 GetBoundingBox(this FrameworkElement source, double scale)
     {
         ArgumentNullException.ThrowIfNull(source);
 
         Rect rect = source
             .TransformToVisual(visual: null)
-            .TransformBounds(
-                new Rect(0, 0, source.ActualWidth, source.ActualHeight)
-            );
+            .TransformBounds(new Rect(0, 0, source.ActualWidth, source.ActualHeight));
 
         scale = scale < 0 ? 1 : scale;
 
