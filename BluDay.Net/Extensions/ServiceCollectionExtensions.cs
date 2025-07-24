ad1a3ee33b707343a0407f0a85a4065013d0f38f
BluDay <19403893+bluday@users.noreply.github.com>
2025-07-24 22:35:05 +0200

    feat: add new factory method extensions for `ServiceCollection`

--- BluDay.Net/Extensions/ServiceCollectionExtensions.cs
+++ BluDay.Net/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Reflection;
 
 namespace BluDay.Net.Extensions;
@@ -17,6 +18,9 @@ public static class ServiceCollectionExtensions
     /// <param name="factory">
     /// A delegate that defines how to configure the service collection.
     /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="source"/> or <paramref name="factory"/> is <c>null</c>.
+    /// </exception>
     /// <returns>
     /// The updated <see cref="IServiceCollection"/> instance.
     /// </returns>
@@ -46,6 +50,10 @@ public static class ServiceCollectionExtensions
     /// <param name="assembly">
     /// The assembly to search for concrete types.
     /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="source"/>, <paramref name="lifetime"/> or <paramref name="assembly"/> is
+    /// <c>null</c>.
+    /// </exception>
     /// <returns>
     /// The updated <see cref="IServiceCollection"/> instance.
     /// </returns>
@@ -68,16 +76,107 @@ public static class ServiceCollectionExtensions
     }
 
     /// <summary>
+    /// Registers a factory delegate for the specified service type that resolves instances using the
+    /// <see cref="IServiceProvider"/> at runtime.
+    /// </summary>
+    /// <typeparam name="TService">
+    /// The service type to register a factory for.
+    /// </typeparam>
+    /// <param name="source">
+    /// The service collection to register the factory with.
+    /// </param>
+    /// <param name="lifetime">
+    /// The service lifetime to apply to the registration.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="source"/> is <c>null</c>.
+    /// </exception>
+    /// <returns>
+    /// The updated <see cref="IServiceCollection"/> instance.
+    /// </returns>
+    public static IServiceCollection AddFactory<TService>(this IServiceCollection source, ServiceLifetime lifetime)
+        where TService : class
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        source.TryAddTransient<TService>();
+
+        Func<IServiceProvider, Func<TService>> implementationFactory =
+            serviceProvider => serviceProvider.GetRequiredService<TService>;
+
+        source.Add(ServiceDescriptor.Describe(
+            serviceType: typeof(Func<TService>),
+            implementationFactory,
+            lifetime
+        ));
+
+        return source;
+    }
+
+    /// <summary>
+    /// Registers a factory delegate for the specified service type as a singleton, using the
+    /// <see cref="IServiceProvider"/> to resolve instances.
+    /// </summary>
+    /// <typeparam name="TService">
+    /// The service type to register as singleton.
+    /// </typeparam>
+    /// <param name="source">
+    /// The service collection to register the singleton factory with.
+    /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="source"/> is <c>null</c>.
+    /// </exception>
+    /// <returns>
+    /// The updated <see cref="IServiceCollection"/> instance.
+    /// </returns>
+    public static IServiceCollection AddFactoryAsSingleton<TService>(this IServiceCollection source)
+        where TService : class
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        return source.AddFactory<TService>(ServiceLifetime.Singleton);
+    }
+
+    /// <summary>
+    /// Registers a factory delegate for the specified service type as transient, using the
+    /// <see cref="IServiceProvider"/> to resolve instances.
+    /// </summary>
+    /// <typeparam name="TService">
+    /// The service type to register as transient.
+    /// </typeparam>
+    /// <param name="source">
+    /// The service collection to register the transient factory with.
+    /// </param>
+    /// <returns>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="source"/> is <c>null</c>.
+    /// </exception>
+    /// The updated <see cref="IServiceCollection"/> instance.
+    /// </returns>
+    public static IServiceCollection AddFactoryAsTransient<TService>(this IServiceCollection source)
+        where TService : class
+    {
+        ArgumentNullException.ThrowIfNull(source);
+
+        return source.AddFactory<TService>(ServiceLifetime.Transient);
+    }
+
+    /// <summary>
     /// Registers all concrete view model types from the calling assembly in the service collection.
     /// </summary>
     /// <param name="source">
     /// The service collection to register view models with.
     /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="source"/> is <c>null</c>.
+    /// </exception>
     /// <returns>
     /// The updated <see cref="IServiceCollection"/> instance.
     /// </returns>
     public static IServiceCollection AddViewModels(this IServiceCollection source)
     {
+        ArgumentNullException.ThrowIfNull(source);
+
         return source.AddViewModels(Assembly.GetCallingAssembly());
     }
 
@@ -91,6 +190,9 @@ public static class ServiceCollectionExtensions
     /// <param name="assembly">
     /// The assembly to search for view model types.
     /// </param>
+    /// <exception cref="ArgumentNullException">
+    /// Thrown if <paramref name="source"/> or <paramref name="assembly"/> is <c>null</c>.
+    /// </exception>
     /// <returns>
     /// The updated <see cref="IServiceCollection"/> instance.
     /// </returns>
